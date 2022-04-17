ec190aad3ea5ac4d405ea593b740cec7c04e4db4
hieunghiaaaaa <101161776+hieunghiaaaaa@users.noreply.github.com>
2022-04-17 10:06:49 +0700

    commit

---
+++ components/users/controller.js
@@ -0,0 +1,27 @@
+// tầng xử lý
+const userService = require('./service');
+const bcrypt = require('bcryptjs');
+
+exports.login = async (email,password) => {
+    const user = await userService.login(email);
+    // if (user && user.password == password) {
+    //     return user;
+    // }
+    // return null;
+    const Check =  await bcrypt.compare(password,user.password);
+    console.log('emaail va pass',email,password)
+    if (!Check) {
+        return null;
+    }
+    return {_id:user._id,email:user.email} 
+}
+
+exports.register = async (email, password, confirm_password) => {
+    if(password != confirm_password) return null;
+    let user = await userService.login(email);
+    if(user) return null;
+
+    const hash = await bcrypt.hash(password, await bcrypt.genSalt(10));
+    user = await userService.register(email, hash);
+    return {_id: user._id };
+}
